572e5e49aed123dbe7c90598f61b0ac8aece7461
Wesley Â.Ferreira <wesleyangeloferreira@gmail.com>
2020-09-29 21:50:34 -0300

    Primeiras aulas curso em video Python

--- EST/Mundo01/aula04.py
+++ EST/Mundo01/aula04.py
@@ -1,2 +1,5 @@
 nome = input('Qual é o seu nome?') 
 idade = input('Qual sua idade?')
+
+
+print("Seu nome é {}, seja bem vindo!".format(nome))
---
+++ EST/Mundo01/aula04/desafio 03.py
@@ -0,0 +1,4 @@
+num1 = int(input("Entre com o primeiro numero: "))
+num2 = int(input("Entre com o segundo numero: "))
+
+print("A soma dos numeros é {} ".format(num1+num2))2
