aa3a20faec0dad26ab933dc03598dec0300aa0f2
Wesley Â.Ferreira <wesleyangeloferreira@gmail.com>
2020-10-02 21:37:47 -0300

    Mais aulas assistida - aula06

---
+++ EST/Mundo01/exercicios/ex004.py
@@ -0,0 +1,9 @@
+x = input('Digite algo e irei suas possibilidades com o IS do pythoon: ')
+
+print(x.isalnum())
+print(x.isalpha())
+print(x.isascii())
+print(x.isdecimal())
+print(x.isdigit())
+print(x.isidentifier())
+print(x.isspace())
