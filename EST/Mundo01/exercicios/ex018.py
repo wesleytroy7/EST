5accdaa1f191bb7071b5cf806a84aa79a2bf7162
Wesley Â.Ferreira <wesleyangeloferreira@gmail.com>
2020-11-02 17:07:26 -0300

    Ajustes no ex0018

--- EST/Mundo01/exercicios/ex018.py
+++ EST/Mundo01/exercicios/ex018.py
@@ -1,5 +1,6 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-import math
+# import math
+from math import tan, sin,cos, radians
 angulo = int(input('Entre com ângulo: '))
 
-print('o Cosseno de {} é {:.3f}, a tangente de {} é {:.3f}'.format(angulo, math.cos(angulo), angulo, math.tan(angulo)))
+print('O seno de {} é {:.2f},\nO Cosseno de {} é {:.2f},\nA tangente de {} é {:.2f}'.format(angulo, sin(radians(angulo)), angulo, cos(radians(angulo)), angulo, tan(radians(angulo))))
