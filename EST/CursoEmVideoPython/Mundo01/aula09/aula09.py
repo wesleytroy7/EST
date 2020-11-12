0c3fa64f4897b958a3661baf14ddff5dc1b5886c
Wesley Â.Ferreira <wesleyangeloferreira@gmail.com>
2020-11-11 23:47:20 -0300

    Criacao do env e outros exer

---
+++ EST/CursoEmVideoPython/Mundo01/aula09/aula09.py
@@ -0,0 +1,21 @@
+frase = 'Wesley & Suzan se AMAM, O SENHOR SÓ UNIU PARA SUA GLORIA!! s2'
+
+sep = '=' *20
+print(sep)
+print(len(frase))
+print(sep)
+print(frase.count('a'))
+print(sep)
+print(frase.find('s'))
+print(sep)
+print(frase.isspace())
+print(frase.rfind('S'))
+print(frase.capitalize())
+print(sep)
+print(frase.lower().find('wesley'))
+print('Lady' in frase)
+print(sep)
+print('W'in frase)
+print(sep)
+print(frase.find('W'))
+print(frase[0:4])
--- EST/CursoEmVideoPython/Mundo01/exercicios/ex021.py
+++ EST/CursoEmVideoPython/Mundo01/exercicios/ex021.py
@@ -22,4 +22,28 @@
 # elif ouvirMusica == '2':
 #     print('jafljs1')
 
-import 
+ #Importando o PyGame
+# import pygame
+# import os
+# # Inicializando o PyGame
+# pygame.init()
+
+# # Carregando o arquivo MP3 e executando
+# if os.path.exists('song.wav'):
+#     pygame.mixer.music.load('/home/wesley/Wtroy/EST/CursoEmVideoPython/Mundo01/exercicios/song.wav')
+#     pygame.mixer.music.play()
+#     pygame.mixer.music.set_volume(1)
+
+#     clock = pygame.time.Clock()
+#     clock.tick(10)
+
+#     while pygame.mixer.music.get_busy():
+#         pygame.event.poll()
+#         clock.tick(10)
+# else:
+#     print('O arquivo musica.mp3 não está no diretório do script Python')
+
+import pygame
+pygame.init()
+pygame.mixer.music.load("/home/wesley/Wtroy/EST/CursoEmVideoPython/Mundo01/exercicios/song.wav")
+pygame.mixer.music.play()
